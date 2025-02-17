66fccf1d8f02dfb30478511d9b8812fd0ddb3784
RaiZeL <satannote7@gmail.com>
2025-02-17 20:41:58 +0700

    Update jualikan.lua

--- jualikan.lua
+++ jualikan.lua
@@ -1,22 +1,27 @@
-local tab_ = {}
+local tab_, sell = {}, false
 
 AddHook('OnVarlist', '@monarchsatan', function(var_t)
-    if var_t[0]:find('OnDialogRequest') and var_t[1]:find('sellfish') then
-      tab_ = {}
-
-      for content in var_t[1]:gmatch('[^\n]+') do
-        if content:find('embed_data') then
-          local embed = { content:match('embed_data|([^|]+)|([^|]+)') }
-          table.insert(tab_t, embed[1])
-          table.insert(tab_t, embed[2])
+        if var_t[0]:find('OnDialogRequest') and var_t[1]:find('sellfish') then
+            tab_ = {}
+            for content in var_t[1]:gmatch('[^\n]+') do
+                if content:find('embed_data') then
+                    local embed = { content:match('embed_data|([^|]+)|([^|]+)') }
+                    table.insert(tab_t, embed[1])
+                    table.insert(tab_t, embed[2])
+                end
+            end
+            return true
         end
-      end 
-      
-      return true
-    end
-  end
-  return false
-end
+        
+        if var_t[0] == 'OnConsoleMessage' and var_t[1]:find('You caught a ```%d+ lb%. .+!``') then
+            local berat, name = var[1]:match('You caught a ```(%d+) lb%. (.+)!``')
+            logToConsole('`w[`4 S A T A N`` ] `oTrying To `2Sell : `9'.. besar ..'. '.. name)
+            sell = true
+        end
+        
+        return false
+    end )
+
 
 function createPacket(str_packet)
   if #tab_ < 8 then
@@ -43,4 +48,17 @@ fish_3746|0
     tab_[5], tab_[6],
     tab_[7], tab_[8]
   )
+end
+
+while true do 
+    sleep(math.random(10, 50) * 10)
+    if sell then
+        sendPacket(
+            2,
+            createPacket('sellfish')
+        )
+        logToConsole('`w[`4 S A T A N`` ] `2Success``!')
+        sell = false
+    end
+    sleep(math.random(10, 50) * 10)
 end
