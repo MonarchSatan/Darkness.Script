5cbeae2dc923d2bf6796868cace9004e05f4b96d
RaiZeL <satannote7@gmail.com>
2025-02-21 21:45:27 +0700

    Update functionShowLastJoin.lua

--- functionShowLastJoin.lua
+++ functionShowLastJoin.lua
@@ -1,10 +1,24 @@
 local darknessTable = {}
 logToConsole('use `5/show`` to check last joined world!')
 
+local function darkness(str)
+    for _, f in ipairs(darknessTable) do
+        if f:find('darkness_'..str..'|') then
+            return _ 
+        end 
+    end
+    return nil
+end
+
 AddHook('OnGameUpdatePacket', 'growtopiascript.my.id', function(darkness)
     if darkness.type == 4 then
       local lastwargh = getWorld().name 
       if lastwargh and lastwargh ~= 'EXIT' and lastwargh ~= '' then
+                local index = darkness(lastwargh)
+                if index then
+                    table.remove(darknessTable, index)
+                end
+                
         table.insert(darknessTable, 'add_button|darkness_'.. lastwargh ..'|`o'.. lasgwargh ..'``|noflags|0|0|')
         if rawlen(darknessTable) >= 20 then
           table.remove(darknessTable, 1)
@@ -16,9 +30,9 @@ AddHook('OnGameUpdatePacket', 'growtopiascript.my.id', function(darkness)
 local function darknessDialog()
   return [[
 set_border_color|112,86,191,255
-set_bg_color|43,34,74,200
+set_bg_color|0,0,0,200
 add_label_with_icon|big|`9Last Joined List|left|3802|
-add_textbox|`#Success Joined : ``]] .. #world_t .. [[ Worlds.|
+add_textbox|`#Success Joined : ``]] .. #darknessTable .. [[ world.|
 add_spacer|small|
 ]] .. table.concat(darknessTable, '\n') .. [[
 add_spacer|small|
